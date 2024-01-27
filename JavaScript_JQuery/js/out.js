7126e19623e946cf5a12dcafaebcf64704398f39
BaekMinSeok <alstjr3834@naver.com>
2024-01-27 15:15:41 +0900

    HTML CSS JS

---
+++ JavaScript_JQuery/js/out.js
@@ -0,0 +1,15 @@
+//out.js
+//함수
+//function 함수명([매개변수]) []:들어올수도있고 안들어올수도있다
+function hello(name)
+{
+    alert("Hello?" +name);
+}
+
+// 함수명 :go
+//매개변수 x
+//기능 : location이용해서 네이버사이트로 이동하는 기능을 완성
+function go()
+{   location.href="http://www.naver.com";
+
+}
